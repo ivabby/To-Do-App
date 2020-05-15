d27ef14c0675cb7542be450e48f20bbd82ff2622
Vaibhav Singh <vaibhavsingh100070@gmail.com>
2020-05-15 17:05:57 +0530

    Delete task , update task, reset all features added

--- js/main.js
+++ js/main.js
@@ -31,8 +31,6 @@ function updateTasks(){
     });
 
 
-
-
     readTasks(completedTaskStore , function(tasks){
         let list = document.getElementById("completed-task-list");
         let innerHTML = "";
@@ -41,7 +39,7 @@ function updateTasks(){
         for(let i = 0; i < Math.min(tasks.length , maxRecentlyDeleted) ; i++) {
             innerHTML += `
                 <li class='invert'>
-                    ${tasks[i].title} : <span>${task[i].completedDate}</span>
+                    ${tasks[i].title} : <span>${tasks[i].completedDate}</span>
                 </li>
             `;
         }
@@ -52,6 +50,35 @@ function updateTasks(){
 
 function onLoad(){
     updateTasks();
+    updateTheme(loadData("toDoTheme"));
+    document.body.style.display = "flex";
+}
+
+function deleteTaskOnClick(elem) {
+    let id = Number(elem.dataset.id);
+    let task = readOneTask(taskStore , id , function(task){
+
+    let completedTask = new CompletedTasks(task.title);
+
+        addTask(completedTaskStore , completedTask , function(){
+            elem.classList.add("exit");
+
+            elem.addEventListener("animationend" , function(){
+                    deleteTask(taskStore , id , function(){
+                        let amountOfTasks = Number(loadData("totalTasks")) - 1;
+                        saveData("totalTasks" , amountOfTasks);
+                        totalTasks.innerHTML = loadData("totalTasks");
+
+                        let amountOfCompleted = Number(loadData("completedTasks")) + 1;
+                        saveData("completedTasks" , amountOfCompleted);
+                        completedTasks.innerHTML = amountOfCompleted;
+
+                        updateTasks();
+                    });
+            });
+
+        });
+    });
 }
 
 input.addEventListener("keydown" , function(e){
@@ -68,3 +95,65 @@ input.addEventListener("keydown" , function(e){
         });
     }
 });
+
+
+function updateTheme(theme) {
+
+    let bgColor = (theme == 'light')? "255,255,255" : "19,19,19";
+    let textColor = (theme == 'light')? "12,12,12" : "255,255,255";
+    let shadowColor = (theme == 'light')? "0,0,0" : "19 ,19 ,19";
+    let grad1 = (theme == 'light')? "108,29,103" : "34,208,163";
+    let grad2 = (theme == 'light')? "100,25,148" : "32,173,211";
+    let sideGrad1 = (theme == 'light')? "255,255,255" : "35,35,35";
+    let sideGrad2 = (theme == 'light')? "251,247,247" : "46,46,46";
+
+
+    let root = document.documentElement;
+
+    root.style.setProperty("--bg-color" , bgColor);
+    root.style.setProperty("--text-color" , textColor);
+    root.style.setProperty("--shadow-color" , shadowColor);
+    root.style.setProperty("--gradient-1" , grad1);
+    root.style.setProperty("--gradient-2" , grad2);
+    root.style.setProperty("--sidebar-gradient-1" , sideGrad1);
+    root.style.setProperty("--sidebar-gradient-2" , sideGrad2);
+
+    document.getElementsByClassName("current-theme")[0].classList.remove("current-theme");
+
+    let activateClass = (theme == 'light')? "light" : "dark";
+    document.getElementById(activateClass).classList.add("current-theme");
+
+    saveData("toDoTheme" , theme);
+
+    let invertStrength = (theme == 'light')? "0%" : "100%";
+
+    let icons = document.getElementsByClassName("icon");
+    for(let i=0;i<icons.length;i++) {
+            icons[i].style.filter = `brightness(100%) invert(${invertStrength})`;
+    }
+
+}
+
+
+
+function attemptReset() {
+    modal.showModal();
+    console.log("Reset");
+}
+
+
+function closeModal() {
+    modal.close();
+}
+
+
+function reset() {
+    console.log("reset");
+    saveData("totalTasks" , 0);
+    totalTasks.innerHTML = "0";
+    saveData("completedTasks" , 0);
+    completedTasks.innerHTML = "0";
+    deleteAllTask(taskStore);
+    deleteAllTask(completedTaskStore);
+    updateTasks();
+}
